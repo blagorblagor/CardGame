52ff618f8374b87df0ccb6c907c71981cfde26a2
blagorblagor <70908201+blagorblagor@users.noreply.github.com>
2020-09-23 22:02:43 +0200

    Initial commit

--- sources/rs/egzebajt/cardgame/playingcards/CardsSimpleGame.java
+++ sources/rs/egzebajt/cardgame/playingcards/CardsSimpleGame.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rs.egzebajt.cardgame.playingcards;
 
 import java.util.ArrayList;
 import java.util.Map;
 
-/**
- *
- * @author goran
- */
 public class CardsSimpleGame extends Cards {
     
     private final int MIN_CARD_VALUE = 1;
